62d4236afdccd9630c95bfbd416a676f782fd325
Rullred <phamtrunghieudhi1301@gmail.com>
2025-04-25 10:40:30 +0700

    MXHSV-27 create LoginForm

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RegisterForm from './pages/RegisterForm'
-
+import LoginForm from './pages/LoginForm'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-    
-
+    <RegisterForm />
+    <LoginForm />
   </StrictMode>,
 )
